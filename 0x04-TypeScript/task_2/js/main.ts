dc923ac1a4fe2e2bbe460975a398c72c3579a2eb
ch3lla <chukwumaemmanuella03@gmail.com>
2023-12-07 00:37:48 +0100

    task 2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,41 +1,55 @@
-interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience: number,
-    location: string
-    [propName: string]: any
+interface DirectorInterface {
+    workFromHome(): string
+
+    getCoffeeBreak(): string
+
+    workDirectorTasks(): string
 }
 
-interface Directors extends Teacher {
-    numberOfReports: number
+interface TeacherInterface {
+    workFromHome(): string
+
+    getCoffeeBreak(): string
+
+    workTeacherTasks(): string
 }
 
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+export class Director implements DirectorInterface {
+
+    workFromHome = (): string => "Working from home";
+
+    getCoffeeBreak = (): string => "Getting a coffee break";
+
+    workDirectorTasks = (): string => "Getting to director tasks";
+
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+export class Teacher implements TeacherInterface {
 
-interface classInterface {
-    workOnHomework(): string,
-    displayName(): string
+    workFromHome = (): string => "Cannot work from home";
+
+    getCoffeeBreak = (): string => "Cannot have a break";
+
+    workTeacherTasks = (): string => "Getting to work";
+
 }
 
-class StudentClass implements classInterface {
-    firstName: string;
-    lastName: string;
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return (employee as Director).workDirectorTasks() !== undefined;
+}
 
-    workOnHomework(): string {
-        return "Currently working";
-    }
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let res = undefined;
+    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
+    return res;
+}
 
-    displayName(): string {
-        return this.firstName;
-    }
+type Subjects = "Math" | "History";
+
+// @ts-ignore
+// @ts-ignore
+export function teachClass(todayClass: Subjects): string | boolean {
+    return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
 }
